b255de0c38744199640bf29a336f1770459b2da4
Mark Lewis <the.mark.lewis@gmail.com>
2025-02-13 21:25:13 -0600

    Create Package@swift-6.0.swift

---
+++ Package@swift-6.0.swift
@@ -0,0 +1,41 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let webAuthPlatforms: [Platform] = [.iOS, .macOS, .macCatalyst, .visionOS]
+let swiftSettings: [SwiftSetting] = [.define("WEB_AUTH_PLATFORM", .when(platforms: webAuthPlatforms))]
+
+let package = Package(
+    name: "Auth0",
+    platforms: [.iOS(.v14), .macOS(.v11), .tvOS(.v14), .watchOS(.v7), .visionOS(.v1)],
+    products: [.library(name: "Auth0", targets: ["Auth0"])],
+    dependencies: [
+        .package(url: "https://github.com/auth0/SimpleKeychain.git", exact:"1.2.0"),
+        .package(url: "https://github.com/auth0/JWTDecode.swift.git", exact:"3.2.0"),
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "7.0.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "13.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "Auth0", 
+            dependencies: [
+                .product(name: "SimpleKeychain", package: "SimpleKeychain"),
+                .product(name: "JWTDecode", package: "JWTDecode.swift")
+            ],
+            path: "Auth0",
+            exclude: ["Info.plist"],
+            resources: [.copy("PrivacyInfo.xcprivacy")],
+            swiftSettings: swiftSettings),
+        .testTarget(
+            name: "Auth0Tests",
+            dependencies: [
+                "Auth0",
+                .product(name: "Quick", package: "Quick"),
+                .product(name: "Nimble", package: "Nimble")
+            ],
+            path: "Auth0Tests",
+            exclude: ["Info.plist", "Auth0.plist"],
+            swiftSettings: swiftSettings)
+    ],
+    swiftLanguageVersions: [.v6]
+)
